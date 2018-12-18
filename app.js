23674dc6778868464d92f737816ecb04c9c78faa
MouadBH <mouad123b@gmail.com>
2018-12-18 00:08:08 +0000

    yeee3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 server = app.listen(port, function () {
   console.log("its running now on " + port + "");
 });
